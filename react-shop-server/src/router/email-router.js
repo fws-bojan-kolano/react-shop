13f43a18af9f390ae9ac812885be4ba8bb8d4424
Bojan Kolano <bojan@forwardslashny.com>
2024-05-15 14:21:00 +0200

    update final

--- react-shop-server/src/router/email-router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { emailController } = require('../controllers/email-controller');
-const emailRouter = express.Router();
-
-emailRouter.post('/email', emailController.emailControllerPost);
-
-module.exports = {
-    emailRouter
-};
--- react-shop-server/src/router/index.js
+++
@@ -1,7 +0,0 @@
-const { usersRouter } = require('./users-router');
-const { productsRouter } = require('./products-router');
-const { emailRouter } = require('./email-router');
-
-module.exports = {
-  router: [usersRouter, productsRouter, emailRouter],
-};
